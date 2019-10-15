e06299625978b4c42f95370223439e727dae0965
Nikita Kanashin <nikita@kanash.in>
2019-10-15 16:49:13 +0300

    eslint + prettier

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-import * as Types from './types'
-import * as Client from './client'
-
-export * from './types'
-export * from './client'
-// export { Types, Client }
+export * from "./types";
+export * from "./client";
--- src/types/query/index.ts
+++ src/types/query/index.ts
@@ -1,39 +1,39 @@
 export interface SearchQuery {
-  per_page?: number,
-  page?: number,
-  query?: string,
-  type?: 'artist' | 'master' | 'release' | 'label',
-  title?: string,
-  release_title?: string,
-  credit?: string,
-  artist?: string,
-  anv?: string,
-  label?: string,
-  genre?: string,
-  style?: string,
-  country?: string,
-  year?: string | number,
-  format?: string,
-  catno?: string,
-  barcode?: string,
-  track?: string,
-  submitter?: string,
-  contributor?: string,
+  per_page?: number;
+  page?: number;
+  query?: string;
+  type?: "artist" | "master" | "release" | "label";
+  title?: string;
+  release_title?: string;
+  credit?: string;
+  artist?: string;
+  anv?: string;
+  label?: string;
+  genre?: string;
+  style?: string;
+  country?: string;
+  year?: string | number;
+  format?: string;
+  catno?: string;
+  barcode?: string;
+  track?: string;
+  submitter?: string;
+  contributor?: string;
 }
 
 export type PaginationOpts = {
-  page?: number,
-  per_page?: number
-}
+  page?: number;
+  per_page?: number;
+};
 
 export type GetArtistReleasesOpts = {
-  sort?: "year" | "title" | "format"
-} & PaginationOpts
+  sort?: "year" | "title" | "format";
+} & PaginationOpts;
 
 export type GetMasterVersionsOpts = {
-  format?: string,
-  label?: string,
-  country?: string,
-  sort?: "released" | "title" | "format" | "label" | "catno" | "country",
-  sort_order?: "asc" | "desc"
-} & PaginationOpts
+  format?: string;
+  label?: string;
+  country?: string;
+  sort?: "released" | "title" | "format" | "label" | "catno" | "country";
+  sort_order?: "asc" | "desc";
+} & PaginationOpts;
